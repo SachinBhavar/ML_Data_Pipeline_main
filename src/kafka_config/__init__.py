a98e13aa9b267d3545ddf0352be07f8c5242912d
SachinBhavar <bhavarsachin@gmail.com>
2022-12-23 23:41:42 +0530

    kafka config changes

--- src/kafka_config/__init__.py
+++ src/kafka_config/__init__.py
@@ -1,17 +1,23 @@
 
 import os
 
+#cloud api details
+API_KEY ="3ZIFEBVHYYI6E6TM"# os.getenv('API_KEY',None)
+API_SECRET_KEY ="64CI6nWCXrcQG3aO/1/akLRJ3fDMw4+LmQTlmSLHFZcpahUuWZbPO4LDolCem2Y6"# os.getenv('API_SECRET_KEY',None)
+BOOTSTRAP_SERVER ="pkc-6ojv2.us-west4.gcp.confluent.cloud:9092"# os.getenv('BOOTSTRAP_SERVER',None)
+
+
+#Authentication related variables
+# SECURITY_PROTOCOL = os.getenv('SECURITY_PROTOCOL',None)
+# SSL_MACHENISM = os.getenv('SSL_MACHENISM',None)
 
 SECURITY_PROTOCOL="SASL_SSL"
 SSL_MACHENISM="PLAIN"
-API_KEY ='3ZIFEBVHYYI6E6TM' # os.getenv('API_KEY',None)
-ENDPOINT_SCHEMA_URL  ='https://psrc-znpo0.ap-southeast-2.aws.confluent.cloud '# os.getenv('ENDPOINT_SCHEMA_URL',None)
-API_SECRET_KEY ='64CI6nWCXrcQG3aO/1/akLRJ3fDMw4+LmQTlmSLHFZcpahUuWZbPO4LDolCem2Y6'# os.getenv('API_SECRET_KEY',None)
-BOOTSTRAP_SERVER ='pkc-6ojv2.us-west4.gcp.confluent.cloud:9092' # os.getenv('BOOTSTRAP_SERVER',None)
-# SECURITY_PROTOCOL = os.getenv('SECURITY_PROTOCOL',None)
-# SSL_MACHENISM = os.getenv('SSL_MACHENISM',None)
-SCHEMA_REGISTRY_API_KEY = '2H7LSZZPSGRFYKAY'#os.getenv('SCHEMA_REGISTRY_API_KEY',None)
-SCHEMA_REGISTRY_API_SECRET ='LMM41CGgNEk27hHCC6hO1WtAts+wug5XNblB3YTKLSDeai0Th+PAu+r2eFrSWJAW' #os.getenv('SCHEMA_REGISTRY_API_SECRET',None)
+
+#schema related variables
+ENDPOINT_SCHEMA_URL  ="https://psrc-znpo0.ap-southeast-2.aws.confluent.cloud"# os.getenv('ENDPOINT_SCHEMA_URL',None)
+SCHEMA_REGISTRY_API_KEY = "4QRMKZMFERF6FLUV"#os.getenv('SCHEMA_REGISTRY_API_KEY',None)
+SCHEMA_REGISTRY_API_SECRET ="nQ24RBCH18FImBn8D2L8s5P4GZQkUzkPQJQK+2lzS088VWPc2ZYNsYX7yHdOIWuU" #os.getenv('SCHEMA_REGISTRY_API_SECRET',None)
 
 
 def sasl_conf():
@@ -24,7 +30,7 @@ def sasl_conf():
                 'sasl.username': API_KEY,
                 'sasl.password': API_SECRET_KEY
                 }
-    print(sasl_conf)
+    #print(sasl_conf)
     return sasl_conf
 
 
